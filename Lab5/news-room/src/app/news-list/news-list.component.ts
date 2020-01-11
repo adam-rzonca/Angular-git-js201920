7a77925ee83963f34a6f703d28752525e9de7c83
Adam Rzońca <adam.rzonca@gmail.com>
2020-01-11 11:45:11 +0100

    Lab5

---
+++ Lab5/news-room/src/app/news-list/news-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { NewsData } from "../news-data";
+import { NewsService } from "../news.service";
+
+@Component({
+  selector: "app-news-list",
+  templateUrl: "./news-list.component.html",
+  styleUrls: ["./news-list.component.scss"]
+})
+export class NewsListComponent implements OnInit {
+  news: NewsData[];
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsService.onChange.subscribe(() => {
+      this.loadNews();
+    });
+
+    this.loadNews();
+  }
+
+  public loadNews() {
+    this.news = [];
+    setTimeout(() => {
+      this.news = this.newsService.getNewsList();
+    }, 2000);
+  }
+}
